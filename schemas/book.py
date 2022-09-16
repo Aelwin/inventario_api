ef6fc8366a4fe0f164130856e3187cf8c7031aa1
Aelwin <mithrandir01@hotmail.com>
2022-09-16 12:29:51 +0200

    sacadas propiedades repetidas a una clase común

--- schemas/book.py
+++ schemas/book.py
@@ -1,12 +1,17 @@
 from datetime import date
+from typing import Optional
 from pydantic import BaseModel
 
 from core.enums.enums import Formato, Idioma
 
-class BookCreate(BaseModel):
+class BookBase(BaseModel) :
     titulo: str
-    precio: float | None
     propietario: str
+    formato: Formato
+    idioma: Idioma
+
+class BookCreate(BookBase):
+    precio: float | None
     isbn: str | None
     sinopsis: str | None
     imagen: str | None
@@ -14,16 +19,26 @@ class BookCreate(BaseModel):
     fecha_compra: date | None
     observaciones: str | None
     categoria: str | None
-    formato: Formato
-    idioma: Idioma
     saga: str | None
     valoracion: int | None
     fecha_inicio_lectura: date | None
     fecha_fin_lectura: date | None
 
-class BookShow(BaseModel) :
-    id: int
-    titulo : str 
+class BookShow(BookBase) :
+    id: int    
+    precio: Optional[float]   
+    isbn: Optional[str]
+    sinopsis: Optional[str]
+    imagen: Optional[str]
+    editorial: Optional[str]
+    fecha_compra: Optional[date]
+    observaciones: Optional[str]
+    categoria: Optional[str]
+    saga: Optional[str]
+    valoracion: Optional[int]
+    fecha_inicio_lectura: Optional[date]
+    fecha_fin_lectura: Optional[date]
+
 
     class Config():  #tells pydantic to convert even non dict obj to json
         orm_mode = True
