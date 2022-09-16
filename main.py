45027e20bce51ecc30b20118d98d29adebc9fd8e
Aelwin <mithrandir01@hotmail.com>
2022-09-16 08:19:22 +0200

    main de la app

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from core.config import settings
+from apis.base import api_router
+from db.session import engine
+from db.session import get_db
+#from db.base import Base
+
+def include_router(app):
+    app.include_router(api_router)
+
+#def configure_static(app):
+   # app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#def create_tables():
+	#Base.metadata.create_all(bind = engine)
+
+def start_application():
+	app = FastAPI(title = settings.PROJECT_NAME, version = settings.PROJECT_VERSION)
+	include_router(app)
+	#configure_static(app)
+	#create_tables()
+	return app
+
+app = start_application()
