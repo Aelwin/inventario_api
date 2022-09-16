00c85ef67c7688f25a7991e6ca97cae3add4ef79
Aelwin <mithrandir01@hotmail.com>
2022-09-16 11:27:02 +0200

    rutas para libros

--- apis/base.py
+++ apis/base.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from apis.version1 import route_homepage
-from apis.version1 import route_author
+from apis.version1 import route_homepage, route_author, route_book
 
 api_router = APIRouter()
 api_router.include_router(route_homepage.router, prefix="", tags=["general_pages"])
 api_router.include_router(route_author.router, prefix="/autores", tags=["autores"])
+api_router.include_router(route_book.router, prefix="/libros", tags=["libros"])
---
+++ apis/version1/route_book.py
@@ -0,0 +1,28 @@
+from http.client import HTTPException
+from fastapi import APIRouter, status
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from typing import List
+
+from db.session import get_db
+from schemas.book import BookCreate, BookShow
+from db.repository.book import crearLibro, recuperarLibro, recuperarLibros
+
+router = APIRouter()
+
+@router.post("/")
+def crear_libro(libro: BookCreate, db: Session = Depends(get_db)):
+    libro = crearLibro(libro = libro, db = db)
+    return libro
+
+@router.get("/{libro_id}", response_model = BookShow)
+def recuperar_libro(libro_id : int, db: Session = Depends(get_db)) :
+    libro = recuperarLibro(libro_id, db)
+    if not libro:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No existe ningún libro con el id {libro_id}")
+    return libro
+
+@router.get("/", response_model = List[BookShow])
+def recuperar_libros(db: Session = Depends(get_db)) :
+    return recuperarLibros(db)
