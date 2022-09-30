618a258a7d27b6231d42d0d33f1650c12724e776
Aelwin <mithrandir01@hotmail.com>
2022-09-30 13:38:56 +0200

    configurar propiedades acceso a través de relación

--- db/models/autor_libros.py
+++ db/models/autor_libros.py
@@ -5,12 +5,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from db.base_class import Base
 
 class Autor_Libros(Base):    
-    libro_id = Column(ForeignKey('books.id'), primary_key=True)
-    autor_id = Column(ForeignKey('authors.id'), primary_key=True)
-    #libro = relationship("Libro", back_populates="autores")
-    #autor = relationship("Autor", back_populates="libros")
+    libro_id = Column(ForeignKey('libro.id'), primary_key=True)
+    autor_id = Column(ForeignKey('autor.id'), primary_key=True)
+    libro = relationship("Libro", back_populates="autores")
+    autor = relationship("Autor", back_populates="libros")
 
-    #proxies
-    #id = association_proxy(target_collection='autor', attr='id')
-    #nombre = association_proxy(target_collection='autor', attr='nombre')
-    #book_title = association_proxy(target_collection='book', attr='title')
+    #proxies    
+    autor_nombre = association_proxy(target_collection='autor', attr='nombre')
+    libro_titulo = association_proxy(target_collection='libro', attr='titulo')
