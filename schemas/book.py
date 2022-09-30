aba1669ce4d15d92190f0b9815afaab5700c1700
Aelwin <mithrandir01@hotmail.com>
2022-09-30 07:54:53 +0200

    mostrar autores del libro

--- schemas/book.py
+++ schemas/book.py
@@ -1,14 +1,16 @@
 from datetime import date
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel
 
 from core.enums.enums import Formato, Idioma
+from schemas.author import AuthorShow
 
 class BookBase(BaseModel) :
     titulo: str
     propietario: str
     formato: Formato
     idioma: Idioma
+    autores: List[AuthorShow]
 
 class BookCreate(BookBase):
     precio: float | None
@@ -39,6 +41,10 @@ class BookShow(BookBase) :
     fecha_inicio_lectura: Optional[date]
     fecha_fin_lectura: Optional[date]
 
-
     class Config():  #tells pydantic to convert even non dict obj to json
         orm_mode = True
+        allow_population_by_field_name = True
+
+class BookResumeShow(BaseModel):
+    id: int
+    titulo: str
