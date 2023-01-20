38e73ad3328288499f77ec9133db16f1dbe81a7c
Aelwin <mithrandir01@hotmail.com>
2023-01-20 12:54:15 +0100

    parametro sslmode

--- db/session.py
+++ db/session.py
@@ -6,7 +6,8 @@ from core.config import settings
 
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# El parámetro sslmode lo necesito para que Deta se conecte a la BBDD en Neon
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'sslmode': "allow"})
 
 #if you don't want to install postgres or any database, use sqlite, a file system based database, 
 # uncomment below lines if you would like to use sqlite and comment above 2 lines of SQLALCHEMY_DATABASE_URL AND engine
