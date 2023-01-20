5bb8171f2039e8dfe8c4c28876e394b6aebc663f
Aelwin <mithrandir01@hotmail.com>
2023-01-20 12:56:19 +0100

    usamos Optional en vez de None

--- schemas/book.py
+++ schemas/book.py
@@ -5,26 +5,28 @@ from pydantic import BaseModel, Field
 from core.enums.enums import Formato, Idioma
 from .author_and_book import BookResumeShow, AuthorResumeShow
 
-class BookBase(BookResumeShow) :
+class BookBase(BookResumeShow):
+    propietario: str
+    formato: Formato
+    idioma: Idioma
+
+class BookCreate(BaseModel):
     titulo: str
     propietario: str
     formato: Formato
     idioma: Idioma
-    #autores: List[AuthorShow]
-
-class BookCreate(BookBase):
-    precio: float | None
-    isbn: str | None
-    sinopsis: str | None
-    imagen: str | None
-    editorial: str | None
-    fecha_compra: date | None
-    observaciones: str | None
-    categoria: str | None
-    saga: str | None
-    valoracion: int | None
-    fecha_inicio_lectura: date | None
-    fecha_fin_lectura: date | None
+    precio: Optional[float]
+    isbn: Optional[str]
+    sinopsis: Optional[str]
+    imagen: Optional[str]
+    editorial: Optional[str]
+    fecha_compra: Optional[date]
+    observaciones: Optional[str]
+    categoria: Optional[str]
+    saga: Optional[str]
+    valoracion: Optional[int]
+    fecha_inicio_lectura: Optional[date]
+    fecha_fin_lectura: Optional[date]
 
 class BookShow(BookResumeShow):
     precio: Optional[float]   
