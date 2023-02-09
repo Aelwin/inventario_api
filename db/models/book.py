bfdbbe450a7d0e82a9ccff14e441bf1522178471
Aelwin <mithrandir01@hotmail.com>
2023-02-09 11:10:04 +0100

    relacion con tabla lectura

--- db/models/book.py
+++ db/models/book.py
@@ -1,8 +1,10 @@
 from sqlalchemy import Column, Integer, String, Float, Date
 from sqlalchemy.orm import relationship
+from typing import List
 
 from db.base_class import Base
 from db.models.autor_libros import Autor_Libros
+from db.models.reading import Lectura
 from schemas.book import BookCreate
 
 class Libro(Base):
@@ -24,6 +26,7 @@ class Libro(Base):
     fecha_inicio_lectura = Column(Date)
     fecha_fin_lectura = Column(Date)
     autores = relationship("Autor_Libros", back_populates="libro")
+    lecturas = relationship("Lectura", back_populates="libro")
 
 def libroFromBookCreate(book: BookCreate):
     libro = Libro(**book.dict(exclude={'autores'}))
