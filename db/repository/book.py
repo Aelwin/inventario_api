ad43fc4d9f7f8c7c4a6b2db27bfaea4215461dbb
Aelwin <mithrandir01@hotmail.com>
2023-03-08 09:56:19 +0100

    modificar autor al actualizar libro

--- db/repository/book.py
+++ db/repository/book.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 
+from db.models.autor_libros import Autor_Libros
 from db.models.book import Libro, libroFromBookCreate
 from schemas.book import BookCreate
 
@@ -20,17 +21,26 @@ def recuperarLibrosPorTitulo(libro_titulo: str, db: Session):
     return db.query(Libro).filter(Libro.titulo.ilike(f"%{libro_titulo}%")).all()
 
 def actualizarLibro(libro_id: int, libro: BookCreate, db: Session) :
-    existing_book = db.query(Libro).filter(Libro.id == libro_id)
-    if not existing_book.first():
-        return 0    
-    existing_book.update(libro.__dict__)
+    existing_book: Libro = recuperarLibro(libro_id, db)
+    if not existing_book:
+        return 0
+    book_data = libro.dict(exclude={"autores"}, exclude_unset=True)
+    for key, value in book_data.items():
+        setattr(existing_book, key, value)
+    autores = []
+    for autor in libro.autores:
+        autores.append(Autor_Libros(autor_id = autor.id))
+    existing_book.autores = autores
+    db.add(existing_book)
     db.commit()
     return 1
 
 def eliminarLibro(libro_id: int, db: Session) :
     existing_book = db.query(Libro).filter(Libro.id == libro_id)
     if not existing_book.first():
-        return 0    
+        return 0
+    existing_relation = db.query(Autor_Libros).filter(Autor_Libros.libro_id == libro_id)
+    existing_relation.delete()
     existing_book.delete(synchronize_session=False)
     db.commit()
     return 1
